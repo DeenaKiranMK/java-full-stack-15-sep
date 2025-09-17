5c00020cc4f9a74154ff8a20742590330c64a2e2
Deena Kiran <2023ci_deenakiranmk_a@nie.ac.in>
2025-09-17 09:42:51 +0530

    errors

--- ecom_server/src/main/java/com/mtd/ecom_server/controllers/ProductController.java
+++ ecom_server/src/main/java/com/mtd/ecom_server/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.mtd.ecom_server.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mtd.ecom_server.models.Product;
 import com.mtd.ecom_server.repos.ProductRepo;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
